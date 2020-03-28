091653c83004f132ca0c399d2c7635ad1cf12321
Rizwan Bashir <rizi.1141@gmail.com>
2020-03-28 20:40:55 +0300

    removed extra key added in prod.js

--- config/prod.js
+++ config/prod.js
@@ -4,6 +4,5 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY,
-    googleRedirectURI: 'https://murmuring-badlands-81838.herokuapp.com' 
+    cookieKey: process.env.COOKIE_KEY
   }
